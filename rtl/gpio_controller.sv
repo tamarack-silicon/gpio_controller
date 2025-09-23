2e955671edaf91c6c0848a52d3ad30f05f3fafbb
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2025-09-22 22:32:15 -0400

    Implement all input and output control bits

--- rtl/gpio_controller.sv
+++ rtl/gpio_controller.sv
@@ -3,7 +3,7 @@ module gpio_controller (
     input logic         sys_clk,
     input logic         rst_n,
     // APB Interface
-    input logic [15:0]  paddr,
+    input logic [11:0]  paddr,
     input logic         pwrite,
     input logic         psel,
     input logic         penable,
@@ -13,9 +13,9 @@ module gpio_controller (
     output logic        pready,
     output logic        pslverr,
     // GPIO interface
-    input logic [31:0]  gpio_in_data,
-    output logic [31:0] gpio_out_data,
-	output logic [31:0] gpio_out_enable
+    input logic [255:0]  gpio_in_data,
+    output logic [255:0] gpio_out_data,
+	output logic [255:0] gpio_out_enable
 );
 
     gpio_ctrl_csr_pkg::gpio_ctrl_csr__in_t csr_in;
@@ -28,7 +28,7 @@ module gpio_controller (
         .s_apb_penable(penable),
         .s_apb_pwrite(pwrite),
         .s_apb_pprot('0),
-        .s_apb_paddr(paddr[3:0]),
+        .s_apb_paddr(paddr[10:0]),
         .s_apb_pwdata(pwdata),
         .s_apb_pstrb(pstrb),
         .s_apb_pready(pready),
@@ -38,7 +38,7 @@ module gpio_controller (
         .hwif_out(csr_out)
     );
 
-    logic [1:0] [31:0] sync_flops;
+    logic [1:0] [255:0] sync_flops;
 
     always_ff @(posedge sys_clk or negedge rst_n) begin
         if(rst_n == 1'b0) begin
@@ -49,8 +49,31 @@ module gpio_controller (
         end
     end
 
-    assign gpio_out_data = csr_out.output_data[0].odata.value;
-	assign gpio_out_enable = csr_out.output_enable[0].oenable.value;
-    assign csr_in.input_data[0].idata.next = sync_flops[1];
+    assign gpio_out_data = {csr_out.output_data[7].odata.value,
+							csr_out.output_data[6].odata.value,
+							csr_out.output_data[5].odata.value,
+							csr_out.output_data[4].odata.value,
+							csr_out.output_data[3].odata.value,
+							csr_out.output_data[2].odata.value,
+							csr_out.output_data[1].odata.value,
+							csr_out.output_data[0].odata.value};
+
+	assign gpio_out_enable = {csr_out.output_enable[7].oenable.value,
+							  csr_out.output_enable[6].oenable.value,
+							  csr_out.output_enable[5].oenable.value,
+							  csr_out.output_enable[4].oenable.value,
+							  csr_out.output_enable[3].oenable.value,
+							  csr_out.output_enable[2].oenable.value,
+							  csr_out.output_enable[1].oenable.value,
+							  csr_out.output_enable[0].oenable.value};
+
+    assign csr_in.input_data[0].idata.next = sync_flops[1][32*0+:32];
+	assign csr_in.input_data[1].idata.next = sync_flops[1][32*1+:32];
+    assign csr_in.input_data[2].idata.next = sync_flops[1][32*2+:32];
+	assign csr_in.input_data[3].idata.next = sync_flops[1][32*3+:32];
+    assign csr_in.input_data[4].idata.next = sync_flops[1][32*4+:32];
+	assign csr_in.input_data[5].idata.next = sync_flops[1][32*5+:32];
+    assign csr_in.input_data[6].idata.next = sync_flops[1][32*6+:32];
+	assign csr_in.input_data[7].idata.next = sync_flops[1][32*7+:32];
 
 endmodule // gpio_controller
--- tb/gpio_controller_tb.sv
+++ tb/gpio_controller_tb.sv
@@ -1,3 +1,95 @@
 module gpio_controller_tb;
 
+    logic        sys_clk;
+    logic        rst_n;
+    logic [11:0] paddr;
+    logic        pwrite;
+    logic        psel;
+    logic        penable;
+    logic [3:0]  pstrb;
+    logic [31:0] pwdata;
+    logic [31:0] prdata;
+    logic        pready;
+    logic		 pslverr;
+
+    logic [255:0] gpio_in_data;
+    logic [255:0] gpio_out_data;
+	logic [255:0] gpio_out_enable;
+
+    gpio_controller u_gpio_controller (.*);
+
+    clocking apb_clk @(posedge sys_clk);
+
+        output paddr;
+        output pwrite;
+        output psel;
+        output penable;
+        output pstrb;
+        output pwdata;
+
+        input  prdata;
+        input  pready;
+        input  pslverr;
+
+    endclocking // apb_clk
+
+    initial begin
+        rst_n = 1'b0;
+
+        paddr = 16'h0;
+        pwrite = 1'b0;
+        psel = 1'b0;
+        penable = 1'b0;
+        pstrb = 4'b1111;
+        pwdata = 32'h0;
+
+        gpio_in_data = 256'h90abcdef00000000;
+
+        #100;
+        rst_n = 1'b1;
+        #200;
+
+        @apb_clk;
+        apb_clk.paddr <= 16'h0;
+        apb_clk.pwrite <= 1'b1;
+        apb_clk.psel <= 1'b1;
+        apb_clk.pwdata <= 32'h12345678;
+        @apb_clk;
+        apb_clk.penable <= 1'b1;
+        @apb_clk;
+        apb_clk.psel <= 1'b0;
+        apb_clk.penable <= 1'b0;
+        @apb_clk;
+
+        #100;
+
+        @apb_clk;
+        apb_clk.paddr <= 16'h204;
+        apb_clk.pwrite <= 1'b0;
+        apb_clk.psel <= 1'b1;
+        @apb_clk;
+        apb_clk.penable <= 1'b1;
+        @apb_clk;
+        apb_clk.psel <= 1'b0;
+        apb_clk.penable <= 1'b0;
+        @apb_clk;
+
+        #200;
+
+        $finish;
+    end
+
+    initial begin
+        sys_clk = 1'b0;
+        forever begin
+            #10;
+            sys_clk = ~sys_clk;
+        end
+    end
+
+    initial begin
+        $dumpfile("gpio_controller_tb.vcd");
+        $dumpvars;
+    end
+
 endmodule // gpio_controller_tb
